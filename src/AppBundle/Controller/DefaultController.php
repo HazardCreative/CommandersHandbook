84de58f3472d77cf8edc7743e73133cfb604e6da
Hazard <michael.niggel@gmail.com>
2017-02-10 14:16:07 -0500

    Begin server-based load and save

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,7 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use AppBundle\Form\EditProfileType;
+use AppBundle\Entity\FrameCompany;
 
 class DefaultController extends Controller {
 	/**
@@ -75,5 +78,132 @@ class DefaultController extends Controller {
         $users = $userManager->findUsers();
 
         return $this->render('player-finder.html.twig', array('users' => $users));
+	}
+
+	/**
+	 * @Route("/manage-conflicts", name="manage-conflicts")
+	 */
+	public function manageConflictsAction(Request $request) {
+		$user = $this->get('security.token_storage')->getToken()->getUser();
+
+		$games_repo = $this->getDoctrine()
+			->getRepository('AppBundle:RAGame');
+
+		$games = $games_repo->findByOwner($user->getId());
+
+        return $this->render('manage-conflicts.html.twig', array(
+			'games' => $games
+		));
+	}
+
+	/**
+	 * @Route("/manage-companies", name="manage-companies")
+	 */
+	public function manageCompaniesAction(Request $request) {
+		$user = $this->get('security.token_storage')->getToken()->getUser();
+
+		$companies_repo = $this->getDoctrine()
+			->getRepository('AppBundle:FrameCompany');
+
+		$companies = $companies_repo->findByOwner($user->getId());
+
+        return $this->render('manage-companies.html.twig', array(
+			'companies' => $companies
+		));
+	}
+
+	/**
+	 * @Route("/save-data", name="save-data")
+	 */
+	public function saveDataAction() {
+		$success_check = true;
+
+		$auth_checker = $this->get('security.authorization_checker');
+
+		if ($auth_checker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+			$user = $this->get('security.token_storage')->getToken()->getUser();
+
+	//		var_dump($_REQUEST); die();
+
+			$result = [];
+			if ($_REQUEST['mfz_companies']) {
+				$data = json_decode($_REQUEST['mfz_companies']);
+
+				$companies_repo = $this->getDoctrine()
+					->getRepository('AppBundle:FrameCompany');
+
+				foreach($data as $incoming) {
+
+					if (!$dbresult = $companies_repo->findById($incoming->id)) {
+						$company = new FrameCompany();
+					} else {
+						$company = $dbresult[0];
+					}
+
+					$company->setTitle($incoming->name);
+	//				$company->setDescription();
+					$company->setOwner($user->getId());
+					$company->setHexcolor($incoming->color);
+					$company->setFrames($incoming->frames);
+	//				$company->setIsShared();
+					$company->setDateModified(new \DateTime());
+
+					try {
+						$em = $this->getDoctrine()->getManager();
+						$em->persist($company);
+						$em->flush();
+
+						$result[] = ['id' => $company->getId()];
+					} catch (Exception $e) {
+						$success_check = false;
+					}
+				}
+			}
+
+			if ($success_check) {
+				return new JsonResponse($result);
+//				return new Response('', Response::HTTP_OK); // 200
+			} else {
+				return new Response('', Response::HTTP_INTERNAL_SERVER_ERROR); // 500
+			}
+		} else {
+			return new Response('Auth Failure', Response::HTTP_INTERNAL_SERVER_ERROR); // 500
+		}
+	}
+
+	/**
+	 * @Route("/load-data", name="load-data")
+	 */
+	public function loadDataAction() {
+		$auth_checker = $this->get('security.authorization_checker');
+
+		if ($auth_checker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+			$user = $this->get('security.token_storage')->getToken()->getUser();
+
+				$companies_repo = $this->getDoctrine()
+					->getRepository('AppBundle:FrameCompany');
+
+				$companies = $companies_repo->findByOwner($user->getId());
+
+				$output = [];
+				foreach ($companies as $company) {
+					$o = [];
+					$o['id'] = $company->getId();
+					$o['name'] = $company->getTitle();
+					$o['description'] = $company->getDescription();
+					$o['owner'] = $user->getUsername();
+					$o['color'] = $company->getHexcolor();
+					$o['frames'] = $company->getFrames();
+					$o['shared'] = $company->getIsShared();
+					$o['created'] = $company->getDateCreated();
+					$o['modified'] = $company->getDateModified();
+					$output[] = $o;
+				}
+
+				return new JsonResponse($output);
+
+		} else {
+			return new Response('Auth Failure', Response::HTTP_INTERNAL_SERVER_ERROR); // 500
+		}
 	}
 }
