846097a0d8bcf92ae11c684038297bd8c66ba6f0
Hazard <michael.niggel@gmail.com>
2017-06-29 22:55:52 -0400

    Implement server-side game sharing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use AppBundle\Form\EditProfileType;
 use AppBundle\Entity\FrameCompany;
+use AppBundle\Entity\RAGame;
 
 class DefaultController extends Controller {
 	/**
@@ -17,6 +18,18 @@ class DefaultController extends Controller {
 	 */
 	public function indexAction() {
 		return $this->render('mfzch/mfzch.html.twig');
+	}
+
+	/**
+	 * @Route("/game/{gameid}/", name="remote-game")
+	 */
+	public function remoteGameAction($gameid) {
+		return $this->render('mfzch/mfzch.html.twig',
+			array(
+				'remotegame' => true,
+				'gameid' => $gameid
+			)
+		);
 	}
 
 	/**
@@ -73,7 +86,7 @@ class DefaultController extends Controller {
 	 * @Route("/player-finder", name="player-finder")
 	 */
 	public function playerFinderAction(Request $request) {
-        return $this->render('player-finder.html.twig');
+		return $this->render('player-finder.html.twig');
 	}
 
 	/**
@@ -130,7 +143,7 @@ class DefaultController extends Controller {
 
 		$games = $games_repo->findByOwner($user->getId());
 
-        return $this->render('manage-conflicts.html.twig', array(
+		return $this->render('manage-conflicts.html.twig', array(
 			'games' => $games
 		));
 	}
@@ -146,7 +159,7 @@ class DefaultController extends Controller {
 
 		$companies = $companies_repo->findByOwner($user->getId());
 
-        return $this->render('manage-companies.html.twig', array(
+		return $this->render('manage-companies.html.twig', array(
 			'companies' => $companies
 		));
 	}
@@ -164,23 +177,24 @@ class DefaultController extends Controller {
 			if (new \DateTime("now") < $user->getEliteExpires()) {
 
 				$result = [];
-				if ($_REQUEST['companies']) {
+				if (isset($_REQUEST['companies']) && $_REQUEST['companies']) {
 					$data = json_decode($_REQUEST['companies']);
 
 					$companies_repo = $this->getDoctrine()
 						->getRepository('AppBundle:FrameCompany');
 
 					foreach($data as $incoming) {
-						if (!$dbresult = $companies_repo->findById($incoming->id)) {
+						if (!$dbresult = $companies_repo->findById($incoming->dbid)) {
 							$company = new FrameCompany();
+							$company->setOwner($user->getId());
 						} else {
 							$company = $dbresult[0];
 						}
 
+						// *** check read/modify permissions
 						if ($incoming->clientmodified) {
 							$company->setTitle($incoming->name);
 							$company->setDescription($incoming->description);
-							$company->setOwner($user->getId());
 							$company->setHexcolor($incoming->color);
 							$company->setFrames($incoming->frames);
 							$company->setIsShared($incoming->shared);
@@ -192,20 +206,74 @@ class DefaultController extends Controller {
 								$em->flush();
 
 								$result[] = array(
-									'id' => $company->getId(),
+									'dbid' => $company->getId(),
 									'name' => $company->getTitle(),
 									'servermodified' => $company->getDateModified(),
 								);
 							} catch (Exception $e) {
 								$success_check = false;
+								break;
 							}
 						} else {
 							$result[] = array(
-								'id' => $company->getId(),
+								'dbid' => $company->getId(),
 								'name' => $company->getTitle(),
 								'servermodified' => $company->getDateModified(),
 							);
 						}
+					}
+				} else if (isset($_REQUEST['game']) && $_REQUEST['game']) {
+					$incoming = json_decode($_REQUEST['game']);
+
+					$game_repo = $this->getDoctrine()
+						->getRepository('AppBundle:RAGame');
+
+					if (!$dbresult = $game_repo->findByOwner($user->getId())) {
+						$game = new RAGame;
+						$game->setOwner($user->getId());
+					} else {
+						$game = $dbresult[0];
+					}
+
+					if ($incoming->clientmodified) {
+						$game->setDescription($incoming->description);
+						$game->setUuid($incoming->uuid);
+						$game->setDoomsday($incoming->doomsday);
+						$game->setRound($incoming->round);
+						$game->setGameType($incoming->gameType);
+						$game->setLog($incoming->log);
+						$game->setMaxFrames($incoming->maxFrames);
+						$game->setMinFrames($incoming->minFrames);
+						$game->setStationsPerPlayer($incoming->stationsPerPlayer);
+						$game->setUnclaimedStations($incoming->unclaimedStations);
+						$game->setTeams($incoming->teams);
+						$game->setTrackingLevel($incoming->trackingLevel);
+						$game->setStartTime($incoming->startTime);
+						$game->setIntendedPlayers($incoming->intendedPlayers);
+						$game->setBlindSetup($incoming->blindSetup);
+						$game->setShared($incoming->shared);
+						$game->setStatus($incoming->status);
+						$game->setViewPassword($incoming->viewPassword);
+						$game->setModifyPassword($incoming->modifyPassword);
+						$game->setDateModified(new \DateTime());
+
+						try {
+							$em = $this->getDoctrine()->getManager();
+							$em->persist($game);
+							$em->flush();
+
+							$result[] = array(
+								'dbid' => $game->getId(),
+								'servermodified' => $game->getDateModified(),
+							);
+						} catch (Exception $e) {
+							$success_check = false;
+						}
+					} else {
+						$result[] = array(
+							'dbid' => $game->getId(),
+							'servermodified' => $game->getDateModified(),
+						);
 					}
 				}
 
@@ -232,34 +300,77 @@ class DefaultController extends Controller {
 			$user = $this->get('security.token_storage')->getToken()->getUser();
 
 			if (new \DateTime("now") < $user->getEliteExpires()) {
-				// if type = ...
-
-				$companies_repo = $this->getDoctrine()
-					->getRepository('AppBundle:FrameCompany');
-
-				$companies = $companies_repo->findByOwner($user->getId());
-
 				$output = array();
 
-				$response = array(
-					'type' => 'company',
-				);
-				foreach ($companies as $company) {
-					$c = array();
-					$c['id'] = $company->getId();
-					$c['name'] = $company->getTitle();
-					$c['description'] = $company->getDescription();
-					$c['owner'] = $user->getUsername();
-					$c['color'] = $company->getHexcolor();
-					$c['frames'] = $company->getFrames();
-					$c['shared'] = $company->getIsShared();
-					$c['created'] = $company->getDateCreated();
-					$c['servermodified'] = $company->getDateModified();
-					$response['data'][] = $c;
-				}
-				$output[] = $response;
+				if (isset($_REQUEST['type'])) {
+					if ($_REQUEST['type'] == 'companies') {
+						$companies_repo = $this->getDoctrine()
+							->getRepository('AppBundle:FrameCompany');
 
-				return new JsonResponse($output);
+						$companies = $companies_repo->findByOwner($user->getId());
+
+						$response = array(
+							'type' => 'companies',
+						);
+						foreach ($companies as $company) {
+							$c = array();
+							$c['dbid'] = $company->getId();
+							$c['name'] = $company->getTitle();
+							$c['description'] = $company->getDescription();
+							$c['owner'] = $user->getUsername(); // db query forces this to be true
+							$c['color'] = $company->getHexcolor();
+							$c['frames'] = $company->getFrames();
+							$c['shared'] = $company->getIsShared();
+							$c['created'] = $company->getDateCreated();
+							$c['servermodified'] = $company->getDateModified();
+							$response['data'][] = $c;
+						}
+
+						$output[] = $response;
+					} else if ($_REQUEST['type'] == 'game') {
+						$game_repo = $this->getDoctrine()
+							->getRepository('AppBundle:RAGame');
+
+						$games = $game_repo->findByOwner($user->getId());
+						$game = $games[0];
+
+						$response = array(
+							'type' => 'game',
+						);
+
+						$g = array();
+						$g['dbid'] = $game->getId();
+						$g['uuid'] = $game->getUUID();
+						$g['description'] = $game->getDescription();
+						$g['owner'] = $user->getUsername(); // db query forces this to be true
+						$g['doomsday'] = $game->getDoomsday();
+						$g['round'] = $game->getRound();
+						$g['gameType'] = $game->getGameType();
+						$g['log'] = $game->getLog();
+						$g['maxFrames'] = $game->getMaxFrames();
+						$g['minFrames'] = $game->getMinFrames();
+						$g['stationsPerPlayer'] = $game->getStationsPerPlayer();
+						$g['unclaimedStations'] = $game->getUnclaimedStations();
+						$g['teams'] = $game->getTeams();
+						$g['trackingLevel'] = $game->getTrackingLevel();
+						$g['startTime'] = $game->getStartTime();
+						$g['intendedPlayers'] = $game->getIntendedPlayers();
+						$g['blindSetup'] = $game->getBlindSetup();
+						$g['shared'] = $game->getShared();
+						$g['status'] = $game->getStatus();
+						$g['viewPassword'] = $game->getViewPassword();
+						$g['modifyPassword'] = $game->getModifyPassword();
+						$g['created'] = $game->getDateCreated();
+						$g['servermodified'] = $game->getDateModified();
+						$response['data'][] = $g;
+
+						$output[] = $response;
+					}
+
+					return new JsonResponse($output);
+				} else {
+					return new Response('Data Error', Response::HTTP_INTERNAL_SERVER_ERROR); // 500
+				}
 			} else {
 				return new Response('Non-elite', Response::HTTP_INTERNAL_SERVER_ERROR); // 500
 			}
@@ -290,7 +401,7 @@ class DefaultController extends Controller {
 					if ($dbresult = $companies_repo->findById($del_id)) {
 						$company = $dbresult[0];
 
-						if ($company->getOwner() == $user->getUsername()) {
+						if ($company->getOwner() == $user->getId()) {
 							try {
 								$em = $this->getDoctrine()->getManager();
 
@@ -298,7 +409,7 @@ class DefaultController extends Controller {
 								$em->flush();
 
 								$result[] = array(
-									'id' => $company->getId(),
+									'dbid' => $company->getId(),
 									'name' => $company->getTitle(),
 									'servermodified' => $company->getDateModified(),
 								);
@@ -325,7 +436,160 @@ class DefaultController extends Controller {
 		}
 	}
 
+	/**
+	 * @Route("/patch-game", name="patch-game")
+	 */
+	public function patchGameAction() {
+		$success_check = true;
 
+		$result = [];
+		if ($_REQUEST['patch']) {
+			$game_dbid = json_decode($_REQUEST['game_dbid']);
+			$data = json_decode($_REQUEST['patch']);
 
+			$game_repo = $this->getDoctrine()
+				->getRepository('AppBundle:RAGame');
 
+			$game = $game_repo->findOneById($game_dbid);
+
+			if ($game) {
+				// patch allowed by owner or anyone with password
+				$grant = false;
+
+				$auth_checker = $this->get('security.authorization_checker');
+				if ($auth_checker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+					$user = $this->get('security.token_storage')->getToken()->getUser();
+					if ($game->getOwner() == $user->getId()) {
+						$grant = true;
+					}
+				}
+
+				if (!$grant
+					&& isset($_REQUEST['password'])
+					&& $game->getModifyPassword() == $_REQUEST['password']) {
+					$grant = true;
+				}
+
+				if ($grant) {
+					foreach($data as $patch) {
+						$game->applyPatch($patch);
+					}
+
+					$game->setDateModified(new \DateTime());
+
+					try {
+						$em = $this->getDoctrine()->getManager();
+						$em->persist($game);
+						$em->flush();
+					} catch (Exception $e) {
+						$success_check = false;
+					}
+
+					$result[] = array( // *** revisit if each is needed
+						'dbid' => $game->getId(),
+						'uuid' => $game->getUUID(),
+						'doomsday' => $game->getDoomsday(),
+						'round' => $game->getRound(),
+						'gameType' => $game->getGameType(),
+						'log' => $game->getLog(),
+						'maxFrames' => $game->getMaxFrames(),
+						'minFrames' => $game->getMinFrames(),
+						'stationsPerPlayer' => $game->getStationsPerPlayer(),
+						'unclaimedStations' => $game->getUnclaimedStations(),
+						'teams' => $game->getTeams(),
+						'trackingLevel' => $game->getTrackingLevel(),
+						'startTime' => $game->getStartTime(),
+						'intendedPlayers' => $game->getIntendedPlayers(),
+						'blindSetup' => $game->getBlindSetup(),
+						'shared' => $game->getShared(),
+						'status' => $game->getStatus(),
+						// 'viewPassword' => $game->getViewPassword(),
+						// 'modifyPassword' => $game->getModifyPassword(),
+						'created' => $game->getDateCreated(),
+						'servermodified' => $game->getDateModified(),
+					);
+
+					return new JsonResponse($result);
+				} else {
+					return new Response('Auth Failure', Response::HTTP_INTERNAL_SERVER_ERROR);
+				}
+			} else {
+				return new Response('No Game', Response::HTTP_INTERNAL_SERVER_ERROR);
+			}
+		} else {
+			return new Response('No Patch', Response::HTTP_INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	/**
+	 * @Route("/remote-load-game/", name="remote-load-game")
+	 */
+	public function loadRemoteGameAction() {
+		if (isset($_REQUEST['type'])
+			&& $_REQUEST['type'] == 'game') {
+
+			$remote = json_decode($_REQUEST['data']);
+
+			$game_repo = $this->getDoctrine()->getRepository('AppBundle:RAGame');
+
+			$game = $game_repo->findOneByUuid( $remote->id );
+
+			if (isset($game)) {
+				if ($game->getModifyPassword() == $remote->password) {
+					$grant = 'modify';
+				} elseif ($game->getViewPassword() == $remote->password) {
+					$grant = 'view';
+				} else {
+					$grant = false;
+				}
+
+				if (!$grant) {
+					$auth_checker = $this->get('security.authorization_checker');
+					if ($auth_checker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+						$user = $this->get('security.token_storage')->getToken()->getUser();
+						if ($game->getOwner() == $user->getId()) {
+							$grant = true;
+						}
+					}
+				}
+
+				if ($grant) {
+					$response = array();
+
+					$response['type'] = 'game';
+
+					$response['data']['dbid'] = $game->getId();
+					$response['data']['uuid'] = $game->getUuid();
+					$response['data']['description'] = $game->getDescription();
+					$response['data']['doomsday'] = $game->getDoomsday();
+					$response['data']['round'] = $game->getRound();
+					$response['data']['gameType'] = $game->getGameType();
+					$response['data']['log'] = $game->getLog();
+					$response['data']['maxFrames'] = $game->getMaxFrames();
+					$response['data']['minFrames'] = $game->getMinFrames();
+					$response['data']['stationsPerPlayer'] = $game->getStationsPerPlayer();
+					$response['data']['unclaimedStations'] = $game->getUnclaimedStations();
+					$response['data']['teams'] = $game->getTeams();
+					$response['data']['trackingLevel'] = $game->getTrackingLevel();
+					$response['data']['startTime'] = $game->getStartTime();
+					$response['data']['intendedPlayers'] = $game->getIntendedPlayers();
+					$response['data']['blindSetup'] = $game->getBlindSetup();
+					$response['data']['shared'] = $game->getShared();
+					$response['data']['status'] = $game->getStatus();
+					$response['data']['created'] = $game->getDateCreated();
+					$response['data']['servermodified'] = $game->getDateModified();
+
+					$response['grant'] = $grant;
+
+					return new JsonResponse($response);
+				} else {
+					return new Response('Auth Failure', Response::HTTP_OK); // 500
+				}
+			} else {
+				return new Response('No Game', Response::HTTP_INTERNAL_SERVER_ERROR); // 500
+			}
+		} else {
+			return new Response('Bad Request', Response::HTTP_INTERNAL_SERVER_ERROR); // 500
+		}
+	}
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -11,307 +11,317 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="fos_user")
  */
 class User extends BaseUser {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
 
-    /**
-     * @ORM\Column(type="string", nullable=true)
-     */
+	/**
+	 * @ORM\Column(type="string", nullable=true)
+	 */
 	public $profile_realname;
 
-    /**
-     * @ORM\Column(type="boolean")
-     */
+	/**
+	 * @ORM\Column(type="boolean")
+	 */
 	public $profile_is_public = false;
 
-    /**
-     * @ORM\Column(type="boolean")
-     */
+	/**
+	 * @ORM\Column(type="boolean")
+	 */
 	public $profile_display_email = false;
 
-    /**
-     * @ORM\Column(type="decimal", precision=8, scale=5, nullable=true)
-     */
+	/**
+	 * @ORM\Column(type="text")
+	 */
+	public $profile_game_preferences;
+
+	/**
+	 * @ORM\Column(type="text")
+	 */
+	public $profile_materials_owned;
+
+	/**
+	 * @ORM\Column(type="decimal", precision=8, scale=5, nullable=true)
+	 */
 	public $geo_latitude;
 
-    /**
-     * @ORM\Column(type="decimal", precision=8, scale=5, nullable=true)
-     */
+	/**
+	 * @ORM\Column(type="decimal", precision=8, scale=5, nullable=true)
+	 */
 	public $geo_longitude;
 
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
+	/**
+	 * @ORM\Column(type="datetime", nullable=true)
+	 */
 	public $elite_expires;
 
-    /**
-     * @ORM\Column(type="integer", nullable=true, unique=true)
-     */
+	/**
+	 * @ORM\Column(type="integer", nullable=true, unique=true)
+	 */
 	public $patreon_id;
 
-    /**
-     * @ORM\Column(type="array", nullable=true)
-     */
+	/**
+	 * @ORM\Column(type="array", nullable=true)
+	 */
 	public $patreon_tokens;
 
-    /**
-     * @ORM\Column(type="array", nullable=true)
-     */
+	/**
+	 * @ORM\Column(type="array", nullable=true)
+	 */
 	public $patreon_pledges;
 
-    /**
-     * @ORM\Column(type="array", nullable=true)
-     */
+	/**
+	 * @ORM\Column(type="array", nullable=true)
+	 */
 	public $patreon_data;
 
-    public function __construct()
-    {
-        parent::__construct();
-        // your own logic
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		// your own logic
+	}
 
-    /**
-     * Set profileDisplayEmail
-     *
-     * @param boolean $profileDisplayEmail
-     *
-     * @return User
-     */
-    public function setProfileDisplayEmail($profileDisplayEmail)
-    {
-        $this->profile_display_email = $profileDisplayEmail;
+	/**
+	 * Set profileDisplayEmail
+	 *
+	 * @param boolean $profileDisplayEmail
+	 *
+	 * @return User
+	 */
+	public function setProfileDisplayEmail($profileDisplayEmail)
+	{
+		$this->profile_display_email = $profileDisplayEmail;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get profileDisplayEmail
-     *
-     * @return boolean
-     */
-    public function getProfileDisplayEmail()
-    {
-        return $this->profile_display_email;
-    }
+	/**
+	 * Get profileDisplayEmail
+	 *
+	 * @return boolean
+	 */
+	public function getProfileDisplayEmail()
+	{
+		return $this->profile_display_email;
+	}
 
-    /**
-     * Set profileRealname
-     *
-     * @param string $profileRealname
-     *
-     * @return User
-     */
-    public function setProfileRealname($profileRealname)
-    {
-        $this->profile_realname = $profileRealname;
+	/**
+	 * Set profileRealname
+	 *
+	 * @param string $profileRealname
+	 *
+	 * @return User
+	 */
+	public function setProfileRealname($profileRealname)
+	{
+		$this->profile_realname = $profileRealname;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get profileRealname
-     *
-     * @return string
-     */
-    public function getProfileRealname()
-    {
-        return $this->profile_realname;
-    }
+	/**
+	 * Get profileRealname
+	 *
+	 * @return string
+	 */
+	public function getProfileRealname()
+	{
+		return $this->profile_realname;
+	}
 
-    /**
-     * Set geoLatitude
-     *
-     * @param string $geoLatitude
-     *
-     * @return User
-     */
-    public function setGeoLatitude($geoLatitude)
-    {
-        $this->geo_latitude = $geoLatitude;
+	/**
+	 * Set geoLatitude
+	 *
+	 * @param string $geoLatitude
+	 *
+	 * @return User
+	 */
+	public function setGeoLatitude($geoLatitude)
+	{
+		$this->geo_latitude = $geoLatitude;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get geoLatitude
-     *
-     * @return string
-     */
-    public function getGeoLatitude()
-    {
-        return $this->geo_latitude;
-    }
+	/**
+	 * Get geoLatitude
+	 *
+	 * @return string
+	 */
+	public function getGeoLatitude()
+	{
+		return $this->geo_latitude;
+	}
 
-    /**
-     * Set geoLongitude
-     *
-     * @param string $geoLongitude
-     *
-     * @return User
-     */
-    public function setGeoLongitude($geoLongitude)
-    {
-        $this->geo_longitude = $geoLongitude;
+	/**
+	 * Set geoLongitude
+	 *
+	 * @param string $geoLongitude
+	 *
+	 * @return User
+	 */
+	public function setGeoLongitude($geoLongitude)
+	{
+		$this->geo_longitude = $geoLongitude;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get geoLongitude
-     *
-     * @return string
-     */
-    public function getGeoLongitude()
-    {
-        return $this->geo_longitude;
-    }
+	/**
+	 * Get geoLongitude
+	 *
+	 * @return string
+	 */
+	public function getGeoLongitude()
+	{
+		return $this->geo_longitude;
+	}
 
-    /**
-     * Set eliteExpires
-     *
-     * @param \DateTime $eliteExpires
-     *
-     * @return User
-     */
-    public function setEliteExpires($eliteExpires)
-    {
-        $this->elite_expires = $eliteExpires;
+	/**
+	 * Set eliteExpires
+	 *
+	 * @param \DateTime $eliteExpires
+	 *
+	 * @return User
+	 */
+	public function setEliteExpires($eliteExpires)
+	{
+		$this->elite_expires = $eliteExpires;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get eliteExpires
-     *
-     * @return \DateTime
-     */
-    public function getEliteExpires()
-    {
-        return $this->elite_expires;
-    }
+	/**
+	 * Get eliteExpires
+	 *
+	 * @return \DateTime
+	 */
+	public function getEliteExpires()
+	{
+		return $this->elite_expires;
+	}
 
-    /**
-     * Set profileIsPublic
-     *
-     * @param boolean $profileIsPublic
-     *
-     * @return User
-     */
-    public function setProfileIsPublic($profileIsPublic)
-    {
-        $this->profile_is_public = $profileIsPublic;
+	/**
+	 * Set profileIsPublic
+	 *
+	 * @param boolean $profileIsPublic
+	 *
+	 * @return User
+	 */
+	public function setProfileIsPublic($profileIsPublic)
+	{
+		$this->profile_is_public = $profileIsPublic;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get profileIsPublic
-     *
-     * @return boolean
-     */
-    public function getProfileIsPublic()
-    {
-        return $this->profile_is_public;
-    }
+	/**
+	 * Get profileIsPublic
+	 *
+	 * @return boolean
+	 */
+	public function getProfileIsPublic()
+	{
+		return $this->profile_is_public;
+	}
 
-    /**
-     * Set patreonId
-     *
-     * @param integer $patreonId
-     *
-     * @return User
-     */
-    public function setPatreonId($patreonId)
-    {
-        $this->patreon_id = $patreonId;
+	/**
+	 * Set patreonId
+	 *
+	 * @param integer $patreonId
+	 *
+	 * @return User
+	 */
+	public function setPatreonId($patreonId)
+	{
+		$this->patreon_id = $patreonId;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get patreonId
-     *
-     * @return integer
-     */
-    public function getPatreonId()
-    {
-        return $this->patreon_id;
-    }
+	/**
+	 * Get patreonId
+	 *
+	 * @return integer
+	 */
+	public function getPatreonId()
+	{
+		return $this->patreon_id;
+	}
 
-    /**
-     * Set patreonTokens
-     *
-     * @param integer $patreonTokens
-     *
-     * @return User
-     */
-    public function setPatreonTokens($patreonTokens)
-    {
-        $this->patreon_tokens = $patreonTokens;
+	/**
+	 * Set patreonTokens
+	 *
+	 * @param integer $patreonTokens
+	 *
+	 * @return User
+	 */
+	public function setPatreonTokens($patreonTokens)
+	{
+		$this->patreon_tokens = $patreonTokens;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get patreonTokens
-     *
-     * @return integer
-     */
-    public function getPatreonTokens()
-    {
-        return $this->patreon_tokens;
-    }
+	/**
+	 * Get patreonTokens
+	 *
+	 * @return integer
+	 */
+	public function getPatreonTokens()
+	{
+		return $this->patreon_tokens;
+	}
 
-    /**
-     * Set patreonPledges
-     *
-     * @param integer $patreonPledges
-     *
-     * @return User
-     */
-    public function setPatreonPledges($patreonPledges)
-    {
-        $this->patreon_pledges = $patreonPledges;
+	/**
+	 * Set patreonPledges
+	 *
+	 * @param integer $patreonPledges
+	 *
+	 * @return User
+	 */
+	public function setPatreonPledges($patreonPledges)
+	{
+		$this->patreon_pledges = $patreonPledges;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get patreonPledges
-     *
-     * @return integer
-     */
-    public function getPatreonPledges()
-    {
-        return $this->patreon_pledges;
-    }
+	/**
+	 * Get patreonPledges
+	 *
+	 * @return integer
+	 */
+	public function getPatreonPledges()
+	{
+		return $this->patreon_pledges;
+	}
 
-    /**
-     * Set patreonData
-     *
-     * @param array $patreonData
-     *
-     * @return User
-     */
-    public function setPatreonData($patreonData)
-    {
-        $this->patreon_data = $patreonData;
+	/**
+	 * Set patreonData
+	 *
+	 * @param array $patreonData
+	 *
+	 * @return User
+	 */
+	public function setPatreonData($patreonData)
+	{
+		$this->patreon_data = $patreonData;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get patreonData
-     *
-     * @return array
-     */
-    public function getPatreonData()
-    {
-        return $this->patreon_data;
-    }
+	/**
+	 * Get patreonData
+	 *
+	 * @return array
+	 */
+	public function getPatreonData()
+	{
+		return $this->patreon_data;
+	}
 
 }
